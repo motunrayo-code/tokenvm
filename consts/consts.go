cbe87016e72a644d2203486e283f0ad7b7a7d01b
motunrayo-code <motunrayosuleiman9@gmail.com>
2024-06-28 19:58:22 +0000

    Motun

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "MOUNTCOIN_HYPERCHAIN"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "MOUNTCOIN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MOUNT"
 )
 
 var ID ids.ID
